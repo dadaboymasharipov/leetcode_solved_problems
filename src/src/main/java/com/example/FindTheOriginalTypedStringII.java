e3860e8e580bcf2e2eebd96a2b062362ae720c9f
dadaboy <dadaboymasharipov@gmail.com>
2025-07-05 09:00:08 +0500

    FindTheOriginalTypedStringII is in progress

---
+++ src/src/main/java/com/example/FindTheOriginalTypedStringII.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class FindTheOriginalTypedStringII {
+
+    /**
+     * 3333. Find the Original Typed String II
+     * HARD
+     */
+    public int possibleStringCount(String word, int k) {
+        return 0;
+    }
+}
